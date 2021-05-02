1391c3f23f5ca700872605d29c5976b3f59b8595
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-05-02 07:33:12 +0000

    [add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +20,24 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # has_many :followeds, through: :followers, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  # has_many :followers, through: :followeds, source: :followed
+  
+  # def follow(other_user)
+  #   unless self == other_user
+  #     self.relationships.find_or_create_by(followed_id: other_user.id)
+  #   end
+  # end
+  # def unfollow(other_user)
+  #   relationship = self.relationships.find_by(followed_id: other_user.id)
+  #   relationship.destroy if relationship
+  # end
   
   attachment :profile_image
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
-  root to: 'homes#top'
+  root to: "homes#top"
   get "home/about" => "homes#about"
   devise_for :users
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
